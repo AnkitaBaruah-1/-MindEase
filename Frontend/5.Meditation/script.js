20c5e418461e3f5978dee55421dca4db97ea8360
AnkitaBaruah-1 <Ankitabaruah23@gmail.com>
2025-03-07 20:10:46 +0530

    Delete Frontend directory

--- Frontend/5.Meditation/script.js
+++
@@ -1,6 +0,0 @@
-
-document.querySelector('.dropdown select').addEventListener('change', function(event) {
-    const selectedCategory = event.target.value;
-    alert(`You selected ${selectedCategory}!`);
-});
-  
